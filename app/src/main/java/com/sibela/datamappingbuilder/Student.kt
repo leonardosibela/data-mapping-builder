ba4b7c5bfff3125ba71f7427cc8678873784d57b
leonardosibela <leonardo_sibela@hotmail.com>
2021-05-02 12:34:51 -0300

    Adding builder pattern

--- app/src/main/java/com/sibela/datamappingbuilder/Student.kt
+++ app/src/main/java/com/sibela/datamappingbuilder/Student.kt
@@ -4,4 +4,24 @@ class Student(
     val name: String?,
     val standard: Int,
     val rollNumber: Int
-)
+) {
+
+    private constructor(builder: Builder) : this(builder.name, builder.standard, builder.rollNumber)
+
+    class Builder {
+        var name: String? = null
+            private set
+        var standard: Int = 0
+            private set
+        var rollNumber: Int = 0
+            private set
+
+        fun name(name: String) = apply { this.name = name }
+
+        fun standard(standard: Int) = apply { this.standard = standard }
+
+        fun rollNumber(rollno: Int) = apply { this.rollNumber = rollNumber }
+
+        fun build() = Student(this)
+    }
+}
